0ce7edab18391892e2f116e0e30c9f8fd255c040
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2023-12-05 14:33:17 -0500

    Update Webpack Configuration -- Fix Errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,45 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  mode: 'development',
+  entry: {
+    main: path.resolve(__dirname, 'src', 'index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(jsx|js|tsx|ts)$/,
+        include: path.resolve(__dirname, 'src'),
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  'targets': 'defaults',
+                },
+              ],
+              '@babel/preset-react',
+            ],
+          },
         },
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
   plugins: [
     new HtmlWebpackPlugin({
