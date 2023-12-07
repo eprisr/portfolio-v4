78de2aa1f03e7d20f7f70a94d0caaf19311ad4c9
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2023-12-07 14:08:43 -0500

    Add Basic CSS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './app'
+import App from './App'
+import './normalize.css'
 
+// if (module.hot) module.hot.accept()
+
+// @ts-ignore
 const root = createRoot(document.getElementById('root'))
 
 root.render(<App />)
