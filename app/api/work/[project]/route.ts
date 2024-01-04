0ac11f31548bb995985af7fcc97a222978dfc4ab
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-01-04 09:08:52 -0500

    Project - Fix - Update Route Folder Name to Match Params

--- app/api/work/[project]/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server'
-import { projectDescs } from '../../../sections/Work/projectDescs'
-
-export async function GET() {
-  return NextResponse.json(projectDescs)
-}
---
+++ app/work/[projectId]/page.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Project from '../../sections/Project/Project'
+import styles from '../../sections/Project/project.module.css'
+
+type ProjIdProps = { params: { projectId: string } }
+
+async function getProjects(projectId: string) {
+  const url =
+    process.env.NODE_ENV === 'development'
+      ? process.env.LOCAL_URL
+      : process.env.PROD_URL
+
+  const res = await fetch(url + `/api/work/${projectId}`)
+
+  if (!res.ok) throw new Error('Failed')
+
+  return res.json()
+}
+
+export default async function page({ params }: ProjIdProps) {
+  const { projectId } = params
+  console.log(projectId)
+  const project = await getProjects(projectId)
+
+  return (
+    <div className="main_wrapper">
+      <Project project={project} />
+    </div>
+  )
+}
