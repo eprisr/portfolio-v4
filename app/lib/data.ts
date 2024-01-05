6b19067d6e1688fc2d5f89742bebb375d617b0c7
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-01-05 13:22:05 -0500

    Add Limit to Project Query

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -3,8 +3,8 @@ import { Project } from './definitions'
 
 export async function fetchProjects() {
   try {
-    const data = await sql<Project>`SELECT * FROM projects`
-    return data.rows
+    const projects = await sql<Project>`SELECT * FROM projects LIMIT 3`
+    return projects.rows
   } catch (error) {
     console.error('Database Error:', error)
     throw new Error('Failed to fetch projects data.')
