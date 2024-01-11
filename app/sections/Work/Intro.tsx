92648becf2e9cf76678d791d178c664b66b901d3
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-01-11 18:37:33 -0500

    Work - Update Intro Style Single Column

--- app/sections/Work/Intro.tsx
+++ app/sections/Work/Intro.tsx
@@ -7,20 +7,22 @@ export default function Intro({}: Props) {
   return (
     <section className={styles.intro}>
       <div className={`${styles.container} container`}>
-        <div className={`${styles.left_column} column`}>
-          <h5 className="sub1">Explore my work</h5>
-          <h2 className="display3">Unlocking Digital Potential.</h2>
-        </div>
-        <div className={`${styles.right_column} column`}>
-          <p>
-            Since 2012, I've worked professionally with several companies across
-            various industries to design and execute accessible, responsive &
-            user-driven websites, decision-making marketing pieces, and a couple
-            of motion graphic videos. No matter the industry or product, I aim
-            to analyze the business' goals, focus on consumers' needs, and
-            deliver quality products and work performance.
-          </p>
-        </div>
+        <h5 className="sub1">Explore my work</h5>
+        <h2 className="display3">
+          Unlocking
+          <br />
+          Digital
+          <br />
+          Potential.
+        </h2>
+        <p>
+          Since 2012, I've worked professionally with several companies across
+          various industries to design and execute accessible, responsive &
+          user-driven websites, decision-making marketing pieces, and a couple
+          of motion graphic videos. No matter the industry or product, I aim to
+          analyze the business' goals, focus on consumers' needs, and deliver
+          quality products and work performance.
+        </p>
       </div>
     </section>
   )
