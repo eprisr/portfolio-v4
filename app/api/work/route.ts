c2e85fa7fd48c1ea3b2156bda9d8e0f34bb51234
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-01-12 20:16:59 -0500

    Delete Route Handlers

--- app/api/work/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server'
-import { fetchProjects } from '../../lib/data'
-
-export async function GET() {
-  const projects = await fetchProjects()
-  return NextResponse.json(projects)
-}
