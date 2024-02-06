e24377f6b1ba708781924deb476bdf58f17b4032
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-02-06 07:28:50 -0500

    Work - Fix - Project Link Overlap Nav

--- app/sections/Project/Projects.tsx
+++ app/sections/Project/Projects.tsx
@@ -21,39 +21,38 @@ export default async function Projects({ total }: { total: number }) {
     <div className={styles.projects_wrapper}>
       <div className={styles.proj_container}>
         {projects.map((project, i) => (
-          <div
-            key={project.id}
-            className={`${i % 2 === 0 ? styles.reverse : ''} ${
-              styles.project
-            }`}>
-            <div className={styles.proj_image}>
-              <Image
-                src={`/assets/images/projects/${project.src}`}
-                alt={project.title}
-                width="660"
-                height="660"
-              />
-            </div>
-            <div className={styles.info}>
-              <p className={`${styles.title} lead_para`}>{project.title}</p>
-              <p className={styles.desc}>{project.brief}</p>
-              <div className={styles.tech_stack}>
-                {Object.keys(project.skills).map(
-                  (skill: string, index: number) =>
-                    project.skills[skill].map((s: string, i: number) => (
-                      <div
-                        key={`${skill}-${index}${i}`}
-                        className={styles.skill}>
-                        <span>{s}</span>
-                      </div>
-                    ))
-                )}
+          <Link href={`/work/${project.id}`} className={styles.proj_link}>
+            <div
+              key={project.id}
+              className={`${i % 2 === 0 ? styles.reverse : ''} ${
+                styles.project
+              }`}>
+              <div className={styles.proj_image}>
+                <Image
+                  src={`/assets/images/projects/${project.src}`}
+                  alt={project.title}
+                  width="660"
+                  height="660"
+                />
+              </div>
+              <div className={styles.info}>
+                <p className={`${styles.title} lead_para`}>{project.title}</p>
+                <p className={styles.desc}>{project.brief}</p>
+                <div className={styles.tech_stack}>
+                  {Object.keys(project.skills).map(
+                    (skill: string, index: number) =>
+                      project.skills[skill].map((s: string, i: number) => (
+                        <div
+                          key={`${skill}-${index}${i}`}
+                          className={styles.skill}>
+                          <span>{s}</span>
+                        </div>
+                      ))
+                  )}
+                </div>
               </div>
             </div>
-            <Link
-              href={`/work/${project.id}`}
-              className={styles.proj_link}></Link>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
