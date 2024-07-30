4aa05041cc24959dde0993d1a36df02ffa2ff6b7
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-07-30 09:03:07 -0400

    Update Recent Project Images

--- app/sections/Home/RecentProjects.tsx
+++ app/sections/Home/RecentProjects.tsx
@@ -15,46 +15,52 @@ function RecentProjects() {
         <div className={styles.project_list}>
           <Link href="/work/maps">
             <div className={styles.project}>
-              <Image
-                src="/assets/images/smoothie_map_3x.png"
-                alt=""
-                width="1140"
-                height="1140"
-              />
               <div className={styles.proj_desc}>
                 <p className={`${styles.lead_para} lead_para`}>
                   Smoothie Locator
                 </p>
                 <p className={styles.language}>React.js</p>
-              </div>
+							</div>
+							<span className={styles.faded_image}>
+								<Image
+									src="/assets/images/smoothie_map_3x.png"
+									alt=""
+									width="1140"
+									height="1140"
+								/>
+							</span>
             </div>
           </Link>
           <Link href="/work/mem">
             <div className={styles.project}>
-              <Image
-                src="/assets/images/memory_game_3x.png"
-                alt=""
-                width="1140"
-                height="1140"
-              />
               <div className={styles.proj_desc}>
                 <p className={`${styles.lead_para} lead_para`}>Memory Game</p>
                 <p className={styles.language}>Vanilla JS</p>
-              </div>
-            </div>
+							</div>
+							<span className={styles.faded_image}>
+								<Image
+									src="/assets/images/memory_game_3x.png"
+									alt=""
+									width="1140"
+									height="1140"
+								/>
+							</span>
+						</div>
           </Link>
           <Link href="/work/reads">
             <div className={styles.project}>
-              <Image
-                src="/assets/images/my_reads_3x.png"
-                alt=""
-                width="1140"
-                height="1140"
-              />
               <div className={styles.proj_desc}>
                 <p className={`${styles.lead_para} lead_para`}>My Reads</p>
                 <p className={styles.language}>React.js</p>
-              </div>
+							</div>
+							<span className={styles.faded_image}>
+								<Image
+									src="/assets/images/my_reads_3x.png"
+									alt=""
+									width="1140"
+									height="1140"
+								/>
+							</span>
             </div>
           </Link>
         </div>
