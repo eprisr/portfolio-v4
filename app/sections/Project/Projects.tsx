706fd534e2810ed65f6f9a1ca81d31dca232431b
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-07-30 21:33:21 -0400

    Update Main Project Images and Style

--- app/sections/Project/Projects.tsx
+++ app/sections/Project/Projects.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 import * as Sentry from '@sentry/nextjs'
 import styles from './projects.module.css'
+import { GithubIcon } from '../../assets/icons'
+import { MdDoubleArrow, MdOpenInNew, MdOpenInNewOff } from 'react-icons/md'
 
 export default async function Projects() {
 	const url = process.env.NEXT_PUBLIC_URL
@@ -21,39 +23,58 @@ export default async function Projects() {
     <div className={styles.projects_wrapper}>
       <div className={styles.proj_container}>
         {projects.map((project, i) => (
-          <Link href={`/work/${project.id}`} className={styles.proj_link}>
-            <div
-              key={project.id}
-              className={`${i % 2 === 0 ? styles.reverse : ''} ${
-                styles.project
-              }`}>
-              <div className={styles.proj_image}>
-                <Image
-                  src={`/assets/images/projects/${project.src}`}
+					<div
+						key={project.id}
+						className={`${i % 2 === 0 ? styles.reverse : ''} ${
+							styles.project
+						}`}>
+						<div className={styles.proj_image}>
+							<span className={styles.faded_image}>
+								<Image
+									src={`/assets/images/projects/${project.src}`}
 									alt={project.title}
 									priority={true}
-                  width="660"
-                  height="660"
-                />
-              </div>
-              <div className={styles.info}>
-                <p className={`${styles.title} lead_para`}>{project.title}</p>
-                <p className={styles.desc}>{project.brief}</p>
-                <div className={styles.tech_stack}>
-                  {Object.keys(project.skills).map(
-                    (skill: string, index: number) =>
-                      project.skills[skill].map((s: string, i: number) => (
-                        <div
-                          key={`${skill}-${index}${i}`}
-                          className={styles.skill}>
-                          <span>{s}</span>
-                        </div>
-                      ))
-                  )}
-                </div>
-              </div>
-            </div>
-          </Link>
+									width="660"
+									height="660"
+								/>
+							</span>
+						</div>
+						<div className={styles.info}>
+							<div className={`${styles.title}`}>
+								<p className='lead_para'>{project.title}</p>
+								<div>
+									<Link href={`/work/${project.id}`} className={styles.proj_link}>
+										Details
+									</Link>
+									<div>
+										<p>Github Repo</p>
+										<Link href={`/work/${project.id}`} className={styles.proj_link}>
+											<MdOpenInNew />
+										</Link>
+									</div>
+								</div>
+							</div>
+							<p className={styles.desc}>{project.brief}</p>
+							<div className={styles.tech_stack}>
+								{Object.keys(project.skills).map(
+									(skill: string, index: number) =>
+										project.skills[skill].map((s: string, i: number) => (
+											<div
+												key={`${skill}-${index}${i}`}
+												className={styles.skill}>
+												<span>{s}</span>
+											</div>
+										))
+								)}
+							</div>
+							<div className={`${styles.open_proj}`}>
+								<Link href={`/work/${project.id}`} className={`${styles.proj_link}`}>
+									Vist Project Website
+									<MdDoubleArrow />
+								</Link>
+							</div>
+						</div>
+					</div>
         ))}
       </div>
     </div>
