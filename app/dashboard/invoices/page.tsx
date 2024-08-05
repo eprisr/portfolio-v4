1f051c9298ad48b93e0a66d79908301f0dc1eccb
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-08-05 05:32:07 -0400

    Create Basic Dashboard Pages

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function page() {
+  return (
+    <section>
+      <div className="container">INVOICES</div>
+    </section>
+  );
+}
+
+export default page;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function page() {
+  return (
+    <section>
+      <div className="container">DASHBOARD</div>
+    </section>
+  );
+}
+
+export default page;
