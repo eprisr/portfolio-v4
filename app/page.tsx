46208f5d98c7f56fbd2b145cf5a61247f1e6ca34
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-08-06 06:40:32 -0400

    Fix - Build Error - Try Calling Data Directly not API Point

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,11 @@
-import React, { Suspense } from 'react';
-import { Project } from '@/app/lib/definitions';
-import * as Sentry from '@sentry/nextjs';
+import React from 'react';
+import { fetchProjects } from './lib/data';
 import Hero from './ui/sections/Home/Hero';
 import RecentProjects from './ui/sections/Home/RecentProjects';
 import Role from './ui/sections/Home/Role';
 
 export default async function Page() {
-  const url = process.env.NEXT_PUBLIC_URL;
-
-  const projects = await fetch(url + '/api/work')
-    .then((res) => res.json())
-    .then((data) => data as Project[])
-    .catch((error) => {
-      console.error('Error Digest:', error.digest);
-      Sentry.captureException(error);
-      Sentry.captureMessage(
-        'An error occured in the App Server Components render',
-      );
-      throw new Error(
-        `An error occured in the App Server Components render ${error}`,
-      );
-    });
+  const projects = await fetchProjects(3, 0, 'All');
 
   return (
     <div className="main_wrapper">
