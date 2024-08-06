b58241b803c2380b251160b3a1cd607722dc98f1
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-08-06 11:00:19 -0400

    Fix - Resume 404 - Try Template Literal

--- app/ui/components/NavBar.tsx
+++ app/ui/components/NavBar.tsx
@@ -71,7 +71,7 @@ export default function NavBar() {
               </NavText>
             ))}
             <NavText
-              href="./Epris_Richardson_Resume.pdf"
+              href={`Epris_Richardson_Resume.pdf`}
               target="_blank"
               rel="noopener noreferrer"
             >
