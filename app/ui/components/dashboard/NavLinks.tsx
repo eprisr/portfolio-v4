fa3fb5c3ac281630595d63181266249837248e0f
Epris Richardson <33326194+eprisr@users.noreply.github.com>
2024-08-08 07:30:04 -0400

    Dashboard - Add Title to Nav Links

--- app/ui/components/dashboard/NavLinks.tsx
+++ app/ui/components/dashboard/NavLinks.tsx
@@ -7,12 +7,17 @@ import { TbFileInvoice, TbHome, TbUsers } from 'react-icons/tb';
 import styles from './navlinks.module.css';
 
 const links = [
-  { name: 'Home', href: '/dashboard', icon: TbHome },
-  { name: 'Projects', href: '/dashboard/projects', icon: TbUsers },
   {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: TbFileInvoice,
+    title: 'Dashboard',
+    links: [
+      { name: 'Home', href: '/dashboard', icon: TbHome },
+      { name: 'Projects', href: '/dashboard/projects', icon: TbUsers },
+      {
+        name: 'Invoices',
+        href: '/dashboard/invoices',
+        icon: TbFileInvoice,
+      },
+    ],
   },
 ];
 
@@ -20,20 +25,25 @@ export default function NavLinks() {
   const pathname = usePathname();
 
   return (
-    <div className={styles.navLinks}>
-      {links.map((link) => {
-        const LinkIcon = link.icon;
-        return (
-          <Link
-            key={link.name}
-            href={link.href}
-            className={`${styles.navLink} ${pathname === link.href && styles.active}`}
-          >
-            <LinkIcon className={styles.linkIcon} />
-            <p className={styles.linkName}>{link.name}</p>
-          </Link>
-        );
-      })}
-    </div>
+    <ul className={styles.navLinks}>
+      {links.map((cat) => (
+        <li key={cat.title}>
+          <span>{cat.title}</span>
+          {cat.links.map((link) => {
+            const LinkIcon = link.icon;
+            return (
+              <Link
+                key={link.name}
+                href={link.href}
+                className={`${styles.navLink} ${pathname === link.href && styles.active}`}
+              >
+                <LinkIcon className={styles.linkIcon} />
+                <p className={styles.linkName}>{link.name}</p>
+              </Link>
+            );
+          })}
+        </li>
+      ))}
+    </ul>
   );
 }
